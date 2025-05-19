311532651ad9abb6d258d147de2ed0303795f64e
favila <favilamontero@gmail.com>
2025-05-19 18:13:41 +0200

    nuevo version del proyecto

---
+++ controlViajesV2/src/main/java/com/rigoV2/controlViajesV2/ControlViajesV2Application.java
@@ -0,0 +1,13 @@
+package com.rigoV2.controlViajesV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlViajesV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlViajesV2Application.class, args);
+	}
+
+}
