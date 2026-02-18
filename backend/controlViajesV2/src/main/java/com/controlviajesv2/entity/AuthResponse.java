d2bd583467f7f9aa14c6f0fe61742f7db9534528
favila <favilamontero@gmail.com>
2026-02-18 16:46:33 +0100

    d

--- backend/controlViajesV2/src/main/java/com/controlviajesv2/entity/AuthResponse.java
+++ backend/controlViajesV2/src/main/java/com/controlviajesv2/entity/AuthResponse.java
@@ -1,10 +1,13 @@
 package com.controlviajesv2.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
-@AllArgsConstructor
+@Data
+@AllArgsConstructor // <-- Esta es la que te falta para aceptar el token en el constructor
+@NoArgsConstructor
 public class AuthResponse {
     private String token;
 }
